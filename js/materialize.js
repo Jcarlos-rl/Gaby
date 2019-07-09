d7a1174783a69632d23a08146c652a6a98bbbea0
Jcarlos-rl <carlos_-619@hotmail.com>
2019-07-09 12:52:45 -0500

    Inicio

---
+++ js/materialize.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.slider').slider({
+        height: 450,
+        indicators: false
+    });
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+});
